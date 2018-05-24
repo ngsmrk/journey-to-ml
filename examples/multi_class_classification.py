f2369768cf847236dc1b4f487b43d50730f185de
Angus <ngsmrk@gmail.com>
2018-05-24 12:57:51 +0100

    Removed classification example

--- examples/multi_class_classification.py
+++ examples/multi_class_classification.py
@@ -11,24 +11,18 @@ from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
 
-data_frame = pd.read_csv('../data/iris.csv')
+data_frame = pd.read_csv('../data/pima-indians-diabetes.csv')
 print(data_frame.columns)
 
-# split by index
-array = data_frame.values
-X = array[:,0:4]
-Y = array[:,4]
+target_column = 'Outcome'
+# split by field name
+X = data_frame[data_frame.columns.drop(target_column)]
+Y = data_frame[target_column].values
 
 print(X.shape)
 print(Y.shape)
 
-# OR split by field name
-X = data_frame[data_frame.columns.drop('Name')]
-Y = data_frame['Name'].values
-
-print(X.shape)
-print(Y.shape)
-
+# how to split data
 validation_size = 0.20
 seed = 7
 X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size, random_state=seed)
@@ -55,15 +49,8 @@ results = []
 names = []
 for name, model in models:
 	kfold = model_selection.KFold(n_splits=10, random_state=seed)
-	cv_results = model_selection.cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
+	cv_results = model_selection.cross_val_score(model, X, Y, cv=kfold, scoring=scoring)
 	results.append(cv_results)
 	names.append(name)
-	msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+	msg = "Name: %s: Mean: %f Std Dev: (%f)" % (name, cv_results.mean(), cv_results.std())
 	print(msg)
-
-	# use confusion matrix to assess performance of model
-	model.fit(X_train, Y_train)
-	predictions = model.predict(X_validation)
-	print(accuracy_score(Y_validation, predictions))
-	print(confusion_matrix(Y_validation, predictions))
-	print(classification_report(Y_validation, predictions))
