a2fd54460fd025f3b4bd62edc3bb61113b134f42
Angus <ngsmrk@gmail.com>
2018-06-04 09:11:16 +0100

    Reorganised folders

---
+++ python_examples/multi_class_classification.py
@@ -0,0 +1,56 @@
+import pandas as pd
+from sklearn import model_selection
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+
+data_frame = pd.read_csv('../data/pima-indians-diabetes.csv')
+print(data_frame.columns)
+
+target_column = 'Outcome'
+# split by field name
+X = data_frame[data_frame.columns.drop(target_column)]
+Y = data_frame[target_column].values
+
+print(X.shape)
+print(Y.shape)
+
+# how to split data
+validation_size = 0.20
+seed = 7
+X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size, random_state=seed)
+
+print(X_train.shape)
+print(X_validation.shape)
+
+print(Y_train.shape)
+print(Y_validation.shape)
+
+
+# Spot Check Algorithms
+scoring = 'accuracy'
+models = []
+models.append(('LR', LogisticRegression()))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC()))
+models.append(('RFC', RandomForestClassifier()))
+# evaluate each model in turn
+results = []
+names = []
+for name, model in models:
+	kfold = model_selection.KFold(n_splits=10, random_state=seed)
+	cv_results = model_selection.cross_val_score(model, X, Y, cv=kfold, scoring=scoring)
+	results.append(cv_results)
+	names.append(name)
+	msg = "Name: %s: Mean: %f Std Dev: (%f)" % (name, cv_results.mean(), cv_results.std())
+	print(msg)
